b57bb80288135bd0cbb8961ca1135e6efead79dc
kit3u <kitsssuni@gmail.com>
2025-11-17 22:12:41 +0200

    gg2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/prac-10/', // Замініть на назву вашого GitHub репозиторію
+  base: '/prak10/', // Замініть на назву вашого GitHub репозиторію
 });
